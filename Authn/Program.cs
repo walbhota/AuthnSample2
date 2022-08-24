28fb1075945099be219daf5da683117fd9f6070d
j8 <walbhota@gmail.com>
2022-08-24 11:59:25 +0100

    Added OpenIdConnect

--- Authn/Program.cs
+++ Authn/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddAuthentication(options =>
     {
         options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-        options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = "GoogleOpenID";
     })
     .AddCookie(
     options =>
@@ -18,16 +18,24 @@ builder.Services.AddAuthentication(options =>
         options.LoginPath = "/login";
         options.AccessDeniedPath = "/denied";
     })
-    .AddGoogle(options =>
+    .AddOpenIdConnect("GoogleOpenID", options =>
     {
+        options.Authority = "https://accounts.google.com";
         options.ClientId = "435073184014-3059uis6u3511jecqsp5atvne5k523n3.apps.googleusercontent.com";
         options.ClientSecret = "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";
         options.CallbackPath = "/auth";
-        //Prompt to consent, to select from list of account after logout, when trying to log back in has been taking cared of in latest .Net
-        //So the AuthorizationEndpoint is not necessary needed, but take note when dealing with other versions.
-        options.AuthorizationEndpoint += "?prompt=consent";
-    }
-    );
+        
+    });
+    //.AddGoogle(options =>
+    //{
+    //    options.ClientId = "435073184014-3059uis6u3511jecqsp5atvne5k523n3.apps.googleusercontent.com";
+    //    options.ClientSecret = "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";
+    //    options.CallbackPath = "/auth";
+    //    //Prompt to consent, to select from list of account after logout, when trying to log back in has been taking cared of in latest .Net
+    //    //So the AuthorizationEndpoint is not necessary needed, but take note when dealing with other versions.
+    //    options.AuthorizationEndpoint += "?prompt=consent";
+    //}
+    //);
 
 
 var app = builder.Build();
